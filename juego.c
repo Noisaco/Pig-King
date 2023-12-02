68a3c8d61012eeb0bcb4856906cf032b042c2605
Noisaco <noisacoav1814@gmail.com>
2023-12-02 19:29:43 +0100

    Estructura
    Creacion de la estructura pirncipal del juego.

--- juego.c
+++ juego.c
@@ -1,11 +1,33 @@
 #include<stdio.h>
+#include<stdbool.h>
+#include"./tigr/tigr.h"
 int menu() {
-    int seleccion;
+    int seleccion=0;
 
+
+    tigrUpdate(screen);
     return seleccion;
+}
+int juego() {
+
+    return 0;
+}
+int selector_de_niveles() {
+
 }
 int main() {
     int caso;
+    int ancho_ventana=800, alto_ventana=800; // Establecer dimensiones de la ventana del juego.
+    Tigr *screen;
+    screen=tigrWindow(ancho_ventana, alto_ventana, "Pig Kong", 0);
+    tigrClear(screen, tigrRGB(0, 0, 0)); // Creacion de la ventana junto con el color de fondo.
     caso=menu();
+    switch(caso) { 
+        case 1:
+                break;
+        case 2: 
+                break;
+    }
+    tigrFree(screen); // Cierre de la ventana de juego.
     return 0;
 }
