89a4f98a6ec1955756349e01103294225925078e
Noisaco <noisacoav1814@gmail.com>
2023-12-02 21:17:37 +0100

    Update juego.c

--- juego.c
+++ juego.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include"./tigr/tigr.h"
-int menu() {
+
+int menu(Tigr *screen) {
     int seleccion=0;
+    bool seleccionado=false;
+    Tigr *raton=;
+    while(seleccionado=false || !tigrClosed(screen) && !tigrKeyHeld(screen, TK_ESCAPE)) {
+    if(tigrKeyHeld(screen, 'Left')) {
 
-
+    }
     tigrUpdate(screen);
+    }
     return seleccion;
 }
 int juego() {
@@ -19,15 +25,15 @@ int main() {
     int caso;
     int ancho_ventana=800, alto_ventana=800; // Establecer dimensiones de la ventana del juego.
     Tigr *screen;
-    screen=tigrWindow(ancho_ventana, alto_ventana, "Pig Kong", 0);
-    tigrClear(screen, tigrRGB(0, 0, 0)); // Creacion de la ventana junto con el color de fondo.
-    caso=menu();
-    switch(caso) { 
-        case 1:
-                break;
-        case 2: 
-                break;
-    }
+    screen=tigrWindow(ancho_ventana, alto_ventana, "Pig Kong", 0); // Abre la ventana del juego
+    tigrClear(screen, tigrRGB(0, 0, 0)); // Estable el color de fondo.
+    caso=menu(screen);
+        switch(caso) {
+            case 1:
+                    break;
+            case 2: 
+                    break;
+        }
     tigrFree(screen); // Cierre de la ventana de juego.
     return 0;
 }
