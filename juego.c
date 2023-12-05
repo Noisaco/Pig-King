2e2144512d48486078be4dfa2a2c3e5781640730
Noisaco <noisacoav1814@gmail.com>
2023-12-05 18:11:32 +0100

    barra menu

--- juego.c
+++ juego.c
@@ -13,33 +13,43 @@ struct coordenadas { // Establecimiento del registro de imagen, coordenadas y di
 int menu(Tigr *screen, int ancho_ventana, int alto_ventana) { // Modulo que muestra el menu.
     struct coordenadas cursor, barra;
     cursor.i=tigrLoadImage("./imagenes/cursor.png"), cursor.x=0, cursor.y=0; // Establecimiento de la posicion anterior del raton.
-    barra.x=0, barra.y, barra.w=ancho_ventana, barra.h=5;
-    int seleccion=0, click, last_click, aceleracion=0;
-    float tiempo;
+    barra.x=0, barra.y=0, barra.w=ancho_ventana, barra.h=5;
+    int seleccion=0, click, last_click, botes=0;
+    float tiempo=0, aceleracion=0;
     bool seleccionado=false, permitido=false, opening=true;
-    TPixel color_morado = tigrRGB(160, 0, 100);
+    TPixel color_morado=tigrRGB(160, 0, 100);
+    TPixel color_azul=tigrRGB(69, 0, 98);
     while(!seleccionado && !tigrClosed(screen) && !tigrKeyHeld(screen, TK_ESCAPE)) {
-//        tigrPrint(screen, tfont, 120, 110, tigrRGB(0, 342, 0), "PIG KING");
-        if (barra.y==2*alto_ventana/8) {
-            aceleracion=20;
+        if (barra.y==2*alto_ventana/8 && botes!=3) { // Control del numero de botes y absorcion de golpe del menu.
+            aceleracion=2.68-botes;
+            botes++;
             tiempo=0;
+            
         }
-        tiempo+=tigrTime(); // Gravedad.
-        barra.y=barra.y+exp(2*tiempo)-aceleracion;
-        if(barra.y>=2*alto_ventana/8) {
+        if(barra.y<=(barra.y+exp(2.7*tiempo)-aceleracion) && aceleracion!=0) {
+            tiempo=0;
+            aceleracion=0;
+        }
+        if(barra.y>2*alto_ventana/8) {
             barra.y=2*alto_ventana/8;
         }
-        tigrClear(screen, tigrRGB(0, 0, 0)); // Limpia la pantalla.
-        tigrFill(screen, barra.x, barra.y, barra.w, barra.h, color_morado);
-        tigrMouse(screen, &cursor.x, &cursor.y, &click); // Comprobacion de si se ha pulsado un boton
+        else if(botes!=3) { // Desplazamiento del menu.
+            barra.y+=exp(2.7*tiempo)-aceleracion;
+        }
         if(click==1 && !opening) {
             if(permitido) {
                 seleccionado=true;
             }
         }
-        tigrBlitAlpha(screen, cursor.i, cursor.x, cursor.y, 0, 0, 32, 32, 1);
+        tigrSetPostFX(screen, 1, 1, 1, 2.0f);
+        tigrClear(screen, tigrRGB(0, 0, 0)); // Limpia la pantalla.
+        tigrFill(screen, 0, 0, barra.w, barra.y, color_azul); // Pintar menu.
+        tigrFill(screen, barra.x, barra.y, barra.w, barra.h, color_morado);
+        tigrPrint(screen, tfont, 120, 110, tigrRGB(224, 13, 40), "PIG KING", 600);
+        tigrMouse(screen, &cursor.x, &cursor.y, &click); // Obtencion de la posicion del cursor y comprobacion de si se ha clicado.
+        tigrBlitAlpha(screen, cursor.i, cursor.x, cursor.y, 0, 0, 32, 32, 1); //Pintar cursor personalizado.
         tigrUpdate(screen);
-        printf("%f", tiempo);
+        tiempo+=tigrTime(); // El tiempo.
     }
     return seleccion;
 }
