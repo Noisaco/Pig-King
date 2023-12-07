a2dc6fc049b6cfa9509ddf421b2c37f820211683
Noisaco <noisacoav1814@gmail.com>
2023-12-07 19:44:24 +0100

    Update juego.c

--- juego.c
+++ juego.c
@@ -2,7 +2,6 @@
 #include <math.h>
 #include<stdbool.h>
 #include"./tigr/tigr.h"
-#define TIGR_NOCURSOR 32
 struct coordenadas { // Establecimiento del registro de imagen, coordenadas y dimensiones de los diferentes elementos del juego.
         Tigr *i;
         int x;
@@ -15,26 +14,22 @@ int menu(Tigr *screen, int ancho_ventana, int alto_ventana) { // Modulo que mues
     cursor.i=tigrLoadImage("./imagenes/cursor.png"), cursor.x=0, cursor.y=0; // Establecimiento de la posicion anterior del raton.
     barra.x=0, barra.y=0, barra.w=ancho_ventana, barra.h=5;
     int seleccion=0, click, last_click, botes=0;
-    float tiempo=0, aceleracion=0;
+    float resguardo=0 , velocidad=0, tiempo_inicial=0, tiempo_final=0, tiempo_total=0, aceleracion=1000;
     bool seleccionado=false, permitido=false, opening=true;
     TPixel color_morado=tigrRGB(160, 0, 100);
     TPixel color_azul=tigrRGB(69, 0, 98);
     while(!seleccionado && !tigrClosed(screen) && !tigrKeyHeld(screen, TK_ESCAPE)) {
-        if (barra.y==2*alto_ventana/8 && botes!=3) { // Control del numero de botes y absorcion de golpe del menu.
-            aceleracion=2.68-botes;
+        tiempo_final=tigrTime();
+        tiempo_total=(tiempo_final-tiempo_inicial);
+        tiempo_inicial=tigrTime();
+        if(resguardo>=2*alto_ventana/8 && velocidad>0 && botes!=4) { // Numero de botes del titulo.
+            velocidad=-velocidad*0.7;
             botes++;
-            tiempo=0;
-            
         }
-        if(barra.y<=(barra.y+exp(2.7*tiempo)-aceleracion) && aceleracion!=0) {
-            tiempo=0;
-            aceleracion=0;
-        }
-        if(barra.y>2*alto_ventana/8) {
-            barra.y=2*alto_ventana/8;
-        }
-        else if(botes!=3) { // Desplazamiento del menu.
-            barra.y+=exp(2.7*tiempo)-aceleracion;
+        if(botes!=4) { // Desplazamiento del titulo.
+            velocidad+=aceleracion*tiempo_total;
+            resguardo+=velocidad*tiempo_total;
+            barra.y=resguardo;
         }
         if(click==1 && !opening) {
             if(permitido) {
@@ -49,7 +44,7 @@ int menu(Tigr *screen, int ancho_ventana, int alto_ventana) { // Modulo que mues
         tigrMouse(screen, &cursor.x, &cursor.y, &click); // Obtencion de la posicion del cursor y comprobacion de si se ha clicado.
         tigrBlitAlpha(screen, cursor.i, cursor.x, cursor.y, 0, 0, 32, 32, 1); //Pintar cursor personalizado.
         tigrUpdate(screen);
-        tiempo+=tigrTime(); // El tiempo.
+        
     }
     return seleccion;
 }
@@ -62,9 +57,9 @@ int selector_de_niveles() {
 }
 int main() {
     int caso;
-    int ancho_ventana=800, alto_ventana=800; // Establecimiento de las dimensiones de la ventana del juego.
+    int ancho_ventana=400, alto_ventana=400; // Establecimiento de las dimensiones de la ventana del juego.
     Tigr *screen;
-    screen=tigrWindow(ancho_ventana, alto_ventana, "Pig Kong", 32); // Abre la ventana del juego
+    screen=tigrWindow(ancho_ventana, alto_ventana, "Pig Kong", TIGR_NOCURSOR | TIGR_2X); // Abre la ventana del juego
     caso=menu(screen, ancho_ventana, alto_ventana);
         switch(caso) {
             case 1:
